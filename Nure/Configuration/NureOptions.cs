b51d2a9395c45c9e93e140098f5df4ade8effda7
Charles-Olivier Magnan <comagnan@coveo.com>
2020-04-15 09:14:10 -0400

    * New parameters added to configuration
    [NURE]

--- Nure/Configuration/NureOptions.cs
+++ Nure/Configuration/NureOptions.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+
 namespace Nure.Configuration
 {
     public class NureOptions
@@ -7,6 +9,8 @@ namespace Nure.Configuration
         public NureOptions()
         {
             AllowPrereleaseDependencies = DEFAULT_ALLOW_PRELEASE_DEPENDENCIES;
+            CustomNugetSources = new List<string>();
+            PackagesToIgnore = new List<string>();
         }
 
         public string HostingService { get; set; }
@@ -14,15 +18,19 @@ namespace Nure.Configuration
         public string NureBranchPrefix { get; set; }
         public string CommitMessage { get; set; }
         public string PullRequestDescription { get; set; }
+        public List<string> CustomNugetSources { get; set; }
+        public List<string> PackagesToIgnore { get; set; }
         public bool AllowPrereleaseDependencies { get; set; }
 
         public override string ToString()
         {
             return $"Hosting Service: {HostingService},\n" +
                 $"Default Branch: {DefaultBranch},\n" +
-                $"Nure Branch Prefix: {NureBranchPrefix},\n" +
+                $"NuRe Branch Prefix: {NureBranchPrefix},\n" +
                 $"Commit Message: {CommitMessage},\n" +
                 $"Pull Request Description: {PullRequestDescription ?? "null"},\n" +
+                $"Custom NuGet Sources: {CustomNugetSources},\n" +
+                $"Packages To Ignore: {PackagesToIgnore},\n" +
                 $"Allow Prerelease Dependencies: {AllowPrereleaseDependencies}\n";
         }
     }
